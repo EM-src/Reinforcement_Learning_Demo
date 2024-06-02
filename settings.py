3065639e9d1043a58bf59c0cd292cae199abb311
Emmanouil Marketos <marketos.manolis@gmail.com>
2024-06-02 13:01:13 +0200

    The game settings
    Various game settings constants etc for the game and level layout

---
+++ settings.py
@@ -0,0 +1,30 @@
+
+level_map = [
+    'XXXXXXXX',
+    'XE     X',
+    'XXX    X',
+    'X   K  X',
+    'X X    X',
+    'X  F   X',
+    'X     PX',
+    'XXXXXXXX'
+] # LEVEL 1
+
+[
+    'XXXXXXXX',
+    'XXX   EX',
+    'XK     X',
+    'X   X  X',
+    'X  XX  X',
+    'X  FX  X',
+    'XP XX  X',
+    'XXXXXXXX'
+] #LEVEL 2
+
+
+BLOCK_SIZE = 50
+WIDTH = 400
+HEIGHT = 400
+MEM_CAPACITY = 100000
+BATCH_SIZE = 1000
+LEARNING_RATE = 0.001
